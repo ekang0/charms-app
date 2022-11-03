71a474492d81ab3039165848a12df7cfa0f4c0b2
Esther Kang <eskang87@gmail.com>
2022-11-03 12:03:00 -0700

    Add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import logo from '../logo.svg';
 import '../App.css';
 import Navbar from './Navbar';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+
+function Header() {
+
+  return (
+    <div className="header">Charms</div>
+  )
+};
+
+export default Header;
