bf85196f486d87a8eaf0344be87698ff5af6901c
Esther Kang <eskang87@gmail.com>
2022-11-04 12:46:51 -0700

    Create SpellCard component

---
+++ src/components/SpellCard.js
@@ -0,0 +1,13 @@
+function SpellCard (spell) {
+
+  return (
+    <div>
+      <h1>{spell.id}</h1>
+      <p>{spell_name}</p>
+      <p>{spell.description}</p>
+      <p>{spell.unforgivable}}</p>
+    </div>
+  )
+};
+
+export default SpellCard;
