d742095d444f802009f1db97d1402687640f7a46
Esther Kang <eskang87@gmail.com>
2022-11-04 12:34:43 -0700

    Create NavLinks and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,28 @@
-const navStyles = {
-  background: "#4c6969"
-}
+import {
+  NavLink
+} from "react-router-dom";
+
 
 function Navbar() {
+  const linkStyles = {
+    background: "#4c6969",
+    padding: "0px",
+    overflow: "auto",
+    color: "white",
+    textDecoration: "none",
+    margin: "0",
+    width: "25%",
+    float: "left",
+  };
+
+
   return(
-    <div className="navbar" style={navStyles}>Navbar works</div>
+    <div className="navbar">
+      <NavLink to="/all" style={linkStyles} activeStyle={{background: "#879f84"}}>all spells</NavLink>
+      <NavLink to="/sinister" style={linkStyles} activeStyle={{background: "black"}}>sinister spells</NavLink>
+      <NavLink to="/add" style={linkStyles} activeStyle={{background: "#879f84"}}>add a spell</NavLink>
+      <NavLink exact to="/" style={linkStyles} activeStyle={{background: "#879f84"}}>home</NavLink>
+    </div>
   )
 };
 
