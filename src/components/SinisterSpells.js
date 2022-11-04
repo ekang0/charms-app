09b9c40d56405d928d537f9e3f398b870457099b
Esther Kang <eskang87@gmail.com>
2022-11-03 17:35:18 -0700

    Create SinisterSpells component

---
+++ src/components/SinisterSpells.js
@@ -0,0 +1,11 @@
+function SinisterSpells() {
+
+  return (
+    <div>
+      <h1>Unforgivable Curses WORKS</h1>
+      <h3>LIST CURSES HERE</h3> 
+    </div>
+  )
+};
+
+export default SinisterSpells;
