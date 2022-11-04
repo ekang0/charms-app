09528a37b97dbc86de5dc2754a09a7d4811cde21
Esther Kang <eskang87@gmail.com>
2022-11-04 12:33:58 -0700

    Add padding to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 const headerStyles = {
-  color: "#bb4f35"
+  color: "#bb4f35",
+  padding: "15px"
 };
 
 
 function Header() {
   return (
-    <div className="header" style={headerStyles}>Charms</div>
+    <div className="header" style={headerStyles}>C h a r m s</div>
   )
 };
 
