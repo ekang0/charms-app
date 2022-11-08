8ff56c652570414328cfa93a722dd660addfc14f
Esther Kang <eskang87@gmail.com>
2022-11-08 12:04:22 -0800

    Create SpellCard component

--- src/components/SpellCard.js
+++ src/components/SpellCard.js
@@ -1,11 +1,36 @@
 function SpellCard (spell) {
+  const {spell: {description}, spell: {id}, spell:{spell_name}, spell: {unforgivable}} = spell;
+
+  
+  const cardStyle = {
+    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+    transition: "0.3s",
+    float: "left",
+    margin: "15px",
+    inlineSize: "300px",
+    overflowWrap: "break-word",
+    background: "#eeeee6",
+    textAlign: "center",
+    overflow: "hidden",
+    textAlign: "justify",
+  };
+
+  const containerStyle = {
+      padding: "30px 35px",
+  }
+
+  const descriptionStyle = {
+    fontSize: "10px"
+  }
+
 
   return (
-    <div>
-      <h1>{spell.id}</h1>
-      <p>{spell_name}</p>
-      <p>{spell.description}</p>
-      <p>{spell.unforgivable}}</p>
+    <div className="card" id={id} style={cardStyle}>
+      <div className="container" style={containerStyle}>
+        <p><b>{spell_name}</b></p>
+        <p style={descriptionStyle}>{description}</p>
+        <p>{unforgivable}</p>
+      </div>
     </div>
   )
 };
