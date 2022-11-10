6be03ab01f5cd7e4e689cc047b941f5c8eec87b0
Esther Kang <eskang87@gmail.com>
2022-11-10 15:03:53 -0800

    Update Navbar Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,30 @@ import {
   NavLink
 } from "react-router-dom";
 
-
 function Navbar() {
+  const navStyle = {
+    maxHeight: "auto",
+    maxWidth: "100%"
+  };
+
   const linkStyles = {
     background: "#4c6969",
-    padding: "0px",
-    overflow: "auto",
-    color: "white",
     textDecoration: "none",
+    color: "white",
+    padding: "0px",
     margin: "0",
     width: "25%",
-    float: "left",
+    overflow: "auto",
+    display: "inline-block"
   };
 
 
   return(
-    <div className="navbar">
+    <div className="navbar" style={navStyle}>
+      <NavLink exact to="/" style={linkStyles} activeStyle={{background: "#879f84"}}>home</NavLink>
       <NavLink to="/all" style={linkStyles} activeStyle={{background: "#879f84"}}>all spells</NavLink>
       <NavLink to="/sinister" style={linkStyles} activeStyle={{background: "black"}}>sinister spells</NavLink>
       <NavLink to="/add" style={linkStyles} activeStyle={{background: "#879f84"}}>add a spell</NavLink>
-      <NavLink exact to="/" style={linkStyles} activeStyle={{background: "#879f84"}}>home</NavLink>
     </div>
   )
 };
