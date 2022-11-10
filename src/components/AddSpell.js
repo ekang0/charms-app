b5feba964a2b29537f88bffb6722860ebd82b695
Esther Kang <eskang87@gmail.com>
2022-11-10 15:02:07 -0800

    Create logic for AddSpell

--- src/components/AddSpell.js
+++ src/components/AddSpell.js
@@ -1,8 +1,49 @@
+import { useState } from "react";
+
 function AddSpell() {
+  const [ formData, setFormData ] = useState({
+    spell_name : "",
+    description : "",
+    unforgivable: null
+  });
+
+  const pTagStyle = {
+    fontSize: "15px"
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //console.log("submit works")
+    //alert("Spell was submitted");
+  };
+
 
   return (
     <div>
-      <h1>ADD SPELL WORKS</h1>
+      <div>
+        <h4 className="h4Style">Add a Spell</h4>
+        <p style={pTagStyle}>Professor Flitwick forgot a spell</p>
+        <p style={pTagStyle}>fill out and submit to add a spell</p>
+      </div>
+      <br></br>
+      <form onSubmit={handleSubmit}>
+        <label>{"spell name "} 
+          <input type="text" ></input>
+        </label>
+        <br></br>
+        <label>{"description "}
+          <input type="text" ></input>
+        </label>
+        <br></br>
+        <label>{"unforgivable "}
+          <select>
+            <option value="false">false</option>
+            <option value="true">true</option>
+          </select>
+        </label>
+        <br></br>
+        <input type="submit" value="Submit"></input>
+      </form>
     </div>
   )
 };
