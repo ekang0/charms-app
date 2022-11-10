e788b8d3fd4a5e986179727686d62415039b098f
Esther Kang <eskang87@gmail.com>
2022-11-10 15:03:04 -0800

    Add Styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-const headerStyles = {
-  color: "#bb4f35",
-  padding: "15px",
-  textAlign: "center",
-  fontSize: "20px",
-};
-
-
 function Header() {
+  const headerStyles = {
+    color: "#bb4f35",
+    padding: "15px",
+    textAlign: "center",
+    fontSize: "20px",
+  };
+
+  
   return (
     <div className="header" style={headerStyles}>C h a r m s</div>
   )
