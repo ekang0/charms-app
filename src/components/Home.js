34a926f3429377bd4cc8fc1e90b5588162b2f5c3
Esther Kang <eskang87@gmail.com>
2022-11-10 15:03:22 -0800

    Add Logic to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
-
 function Home() {
+  const pTagStyle = {
+    fontSize: "15px"
+  };
 
+  const imageStyle = {
+    borderRadius: "20px",
+    padding: "0",
+    display: "block",
+    margin: "0 auto",
+    maxHeight: "auto",
+    maxWidth: "95%",
+    height: "auto"
+  };
+
+  
   return (
     <div>
-      <h1>HOME WORKS</h1>
-      <h3>wands at the ready</h3>
+      <h4 className="h4Style">W E L C O M E</h4>
+      <p style={pTagStyle}>wands at the ready</p>
+      <img style={imageStyle} width="auto" height="auto" src="https://images.ctfassets.net/usf1vwtuqyxm/23IGLVpSUYoES0mowGqOqC/09d766667b4f0ed30eabf31e3f0df919/WingardiumLeviosa_WB_F1_FlitwickTeachesCharmsClass_00356404.jpg?fm=jpg&q=70&w=2560"/>
+      <p>Welcome students to Hogwarts Professor Flitwick's online charms guide.</p>
+      <p>Hogwarts Professor Flitwick has decided to teach charms online.</p>
+      <p>Students wands at the ready. Let's begin.</p>
     </div>
   )
 };
