f0a5f85227c650dd670a60814e0aa9b0fbdf9ab4
Esther Kang <eskang87@gmail.com>
2022-11-16 11:01:30 -0800

    Add Quotes to Text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 function Footer() {
-  const footerStyle ={
+  const footerStyle = {
     background: "#4c6969",
     marginTop: "80px",
     color: "white",
     fontSize: "15px",
     bottom: "0"
-  }
+  };
+
 
   return (
     <div className="footer" style={footerStyle}>
-      <p>Working hard is important. But there is something that matters even more: </p>
-      <h3 style={{color: "#879f84"}}>Believing in yourself.</h3>
+      <p>"Working hard is important. But there is something that matters even more: </p>
+      <h3 style={{color: "#879f84"}}>Believing in yourself." - Harry Potter</h3>
     </div>
   )
 };
