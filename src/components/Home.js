d8390bb13e622d3fa53e70a769b917062eb34637
Esther Kang <eskang87@gmail.com>
2022-12-08 11:45:32 -0800

    Add Alt Text to Image

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ function Home() {
     <div>
       <h4 className="h4Style">W E L C O M E</h4>
       <p style={pTagStyle}>wands at the ready</p>
-      <img style={imageStyle} width="auto" height="auto" src="https://images.ctfassets.net/usf1vwtuqyxm/23IGLVpSUYoES0mowGqOqC/09d766667b4f0ed30eabf31e3f0df919/WingardiumLeviosa_WB_F1_FlitwickTeachesCharmsClass_00356404.jpg?fm=jpg&q=70&w=2560"/>
+      <img style={imageStyle} width="auto" height="auto" alt="Professor Flitwick's charms class practicing levitation spell" src="https://images.ctfassets.net/usf1vwtuqyxm/23IGLVpSUYoES0mowGqOqC/09d766667b4f0ed30eabf31e3f0df919/WingardiumLeviosa_WB_F1_FlitwickTeachesCharmsClass_00356404.jpg?fm=jpg&q=70&w=2560"/>
       <p>Welcome students to Hogwarts Professor Flitwick's online charms guide.</p>
       <p>Students, wands at the ready. Let's begin!</p>
     </div>
